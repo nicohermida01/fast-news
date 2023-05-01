c4a93ca62013bd25f53e4892780f621abf764ceb
nicohermida01 <nshermida101@gmail.com>
2023-05-01 20:21:38 -0300

    app init

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const constants = {
+	APP_MAX_WIDTH: 1300,
+}
---
+++ src/interfaces/article.interface.ts
@@ -0,0 +1,11 @@
+export interface IArticle {
+	author: string
+	title: string
+	description: string
+	source: string
+	image: string
+	category: string
+	country: string
+	language: string
+	published_at: Date | string
+}
